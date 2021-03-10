166ce7974b6170ac9fb648896abc2bbfa22c8ed1
Aitem <muradbei@gmail.com>
2021-03-10 11:35:53 +0300

    After session

--- postgresql/playground.sql
+++ postgresql/playground.sql
@@ -1,4 +1,57 @@
 ---- db: -h localhost -p 5400 -U postgres postgres
+---- db: -h localhost -p 5439 -U postgres testbox
+with recursive
+items as
+(
+  select ARRAY[t.k] pth, t.v val
+  from organization
+     , jsonb_each(resource) t(k,v)
+  union
+  select pth || ARRAY[t.k] , t.v val
+  from items, jsonb_each(val) t(k, v)
+  where jsonb_typeof(val) = 'object'
+)
+, total as
+(
+  select count(*) t from organization
+)
+select pth, count(*) cnt , (count(*)::float / (select t from total))
+from items
+group by pth
+order by cnt desc
+
+----
+limit 0;
+
+
+----
+
+select k.*
+from (select  resource from practitioner limit 1) t(resource),
+jsonb_each(resource)  k(k,v)
+;
+
+----
+
+
+select jsonb_pretty(resource)
+from practitioner
+limit 10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----
 -- TCL block samples
 
 select * from sample;
