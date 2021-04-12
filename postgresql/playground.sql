a8f44d2d889d89fa3168616becc9dcd1b1b37060
Aitem <muradbei@gmail.com>
2021-04-12 16:03:38 +0300

    Style

--- postgresql/playground.sql
+++ postgresql/playground.sql
@@ -1,65 +1,13 @@
 ---- db: -h localhost -p 5400 -U postgres postgres
 
----- db: -h localhost -p 5439 -U postgres testbox
-with recursive
-items as
-(
-  select ARRAY[t.k] pth, t.v val
-  from organization
-     , jsonb_each(resource) t(k,v)
-  union
-  select pth || ARRAY[t.k] , t.v val
-  from items, jsonb_each(val) t(k, v)
-  where jsonb_typeof(val) = 'object'
-)
-, total as
-(
-  select count(*) t from organization
-)
-select pth, count(*) cnt , (count(*)::float / (select t from total))
-from items
-group by pth
-order by cnt desc
-
-----
-
-
-----
-
-select k.*
-from (select  resource from practitioner limit 1) t(resource),
-jsonb_each(resource)  k(k,v)
-;
-
-----
-
-
-select jsonb_pretty(resource)
-from practitioner
-limit 10;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-----
 -- TCL block samples
 
 select * from sample;
-
+----
 ----
 begin;
 insert into sample (id)
-select * from generate_series(0, 10);
+select i from generate_series(0, 10) t(i);
 commit;
 
 ----
